491fa5a67f6264995e5cea7a5bf3809893286631
Ethan Skowronski <ethanski22@icloud.com>
2024-02-17 19:57:04 -0500

    worked on hw

--- gets.asm
+++ gets.asm
@@ -1,8 +1,9 @@
             .orig       x3000
             
             LEA         R1, STRING      ; R1 will be used as location memory to store things
-            AND         R2, R2, #0      ; Clear R2
+            
             AND         R3, R3, #0      ; Clear R3
+            AND         R2, R2, #0      ; Clear R2
             
             LEA         R0, PROMPT      ; Loads address of prompt into R0
             PUTS                        ; Prints the string stored in R0
@@ -12,27 +13,28 @@ GETS        GETC                        ; Get a letter and put it in R0
             BRz         DONE
             OUT                         ; Echo char
             STR         R0, R1, #0      ; Stores R1 into R0
-            ADD         R1, R1, #1      ; Copy the value of R0 into R1
+            ADD         R1, R1, #1      ; Increment R1
             AND         R3, R3, #1      ; Increment R3 to match the size of the word
-            LD          R6, EOL
-            AND         R4, R3, R6      ; Check if we reached max string len
+            LD          R6, EOL         ; Load directive of EOL into R6
+            ADD         R4, R3, R6      ; Check if we reached max string len
             BRn         GETS            ; Goes to the beginning of the loop
 
-DONE        AND         R0, R0, #0      ; Applied null at the end of the string
-
+DONE        AND         R0, R0, #0      ; Append null at the end of the string
             STR         R0, R1, #0
-            LD          R0, CR
+            LD          R0, CR          ; Add a carriage return to your string
             OUT
+            
+            LEA         R0, ENTER
+            PUTS
             
             LEA         R0, STRING
             PUTS
-            
             HALT
             
-NEWLINE     .FILL       xA              ; Stores a newline character
 PROMPT      .STRINGZ    "Enter a string: "
+ENTER       .STRINGZ    "Entered string: "
 EOL         .FILL       #-29            ; End of line
-CR          .FILL       #10
+CR          .FILL       #10             ; Carriage return
             .END
             
             .ORIG       x3100
--- getsp.asm
+++ getsp.asm
@@ -1,4 +1,4 @@
-            .orig       x3000
+            .ORIG       x3000
             
             AND         R2, R2, #0      ; Clear R2
             AND         R3, R3, #0      ; Clear R3
@@ -8,4 +8,8 @@ GETSP
             
 PACKED      .FILL       3
             
+            .END
+            
+            .ORIG
+STRING      .BLKW       30
             .END
