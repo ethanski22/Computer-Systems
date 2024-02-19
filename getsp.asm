794bc1d649cf49d6eb1b6cc12b5bd36b2d9394c4
Ethan Skowronski <ethanski22@icloud.com>
2024-02-19 18:05:16 -0500

    "completed" getsp
    the first 2 chars are printing as they should be but after that IDK whats happening

--- getsp.asm
+++ getsp.asm
@@ -1,10 +1,12 @@
             .ORIG       x3000
             
-            LD         R1, STRING       ; R1 will be used as memory location to store the packed string
+            LD          R1, STRING       ; R1 will be used as memory location to store the packed string
             
                                         ; Uses these 2 registers to store each pair of chars
             AND         R2, R2, #0      ; Clear R2 for the first char
             AND         R3, R3, #0      ; Clear R3 for the second char
+            AND         R4, R4, #0      ; Clear R4 for packedchar
+            AND         R5, R5, #0      ; Clear R5 for checker
             
             LEA         R0, PROMPT      ; Loads prompt into R0
             PUTS        
@@ -13,9 +15,15 @@ GETSP       GETC                        ; Gets one char from user input
             ADD         R2, R2, R0      ; Copy the value inputed into R2
             OUT                         ; Prints out what the user typed
             
+            ADD         R5, R0, #-10    ; Checks for newline char
+            BRz         ENDLOOP         ; If it is close loop
+            
             GETC                        ; Gets the next char from user input
             ADD         R3, R3, R0      ; Copy the next value inputed into R3
             OUT                         ; Prints out what the user typed
+            
+            ADD         R5, R0, #-10    ; Checks for newline char
+            BRz         ENDLOOP         ; If it is close loop
             
                                         ; Puts R3 into the first 8 bits
             ADD         R3 R3 R3        ; hacked shift left 1
@@ -27,12 +35,17 @@ GETSP       GETC                        ; Gets one char from user input
             ADD         R3 R3 R3        ; hacked shift left 7 
             ADD         R3 R3 R3        ; hacked shift left 8
             
-            ADD         R4, R3, R2      ; Creates a packed string
+            ADD         R4, R3, R2      ; Packs the chars into R4
             
-            STR         R4, R1, #0      ; Put R1 into R4 and add 0
+            STR         R4, R1, #0      ; Move the contents of R4 to the address stored in R1
             ADD         R1, R1, #1      ; Incriment the R1 pointer
             
-            BRz         GETSP           ; Go to beginning of the loop
+            LD          R5, NEWLINE     ; Load the ascii code of newline
+            ADD         R3, R3, R5      ; Checks if R2 is a newline char
+            
+            BRnp        GETSP           ; Go to beginning of the loop
+            
+ENDLOOP
             
             AND         R0, R0, #0      ; Clear out R0
             STR         R0, R1, #1      
@@ -45,7 +58,6 @@ GETSP       GETC                        ; Gets one char from user input
             
             LD          R0, STRING      ; Loads string into R0
             PUTSP
-            
             
             HALT
             
