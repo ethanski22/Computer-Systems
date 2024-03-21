5487179fdc17498df4fd90bb42c4e56af08a361d
Ethan Skowronski <ethanski22@icloud.com>
2024-03-21 16:56:27 -0400

    Completed hw 4
    I gave up

--- Assembly/mainHW4.asm
+++ Assembly/mainHW4.asm
@@ -1,4 +1,6 @@
             .ORIG       x3000
+
+START
                                         ; Get starting address
             LEA         R0, PROMPT1     ; Stores the contents of prompt1 into R0
             PUTS                        ; Prints out prompt1 that is stored in R0
@@ -12,6 +14,17 @@
             PUTS                        ; Prints out prompt2 that is stored in R0
             TRAP        x40             ; Reads a 4 digit hex and returns it in R0
             ST          R0, INPUT2      ; Store the second input
+            
+            ; Check to see if input1 is smaller than input 2
+            
+            LD          R1, INPUT1
+            LD          R2, INPUT2
+            NOT         R1, R1          ; Twos compliment
+            ADD         R1, R1, #1      ; Negative
+            ADD         R3, R2, R1      ; Adds R2 and neg R1
+            BRn         NOTPOSITIVE     ; The second input is smaller than the first
+                                        ; Else move on
+            
             
             ; Print out the next line of text
             ; "Memory contents x____ to x____:"
@@ -37,7 +50,32 @@
             LD          R3, INPUT2
             
 LOOP            
+            ; First lets make sure the storage
+            ; variable is cleared before we
+            ; add anything to it
+            
+            LD          R5, CLEARVALUE
+            ST          R5, STORE
+            
             TRAP        x41             ; Display the users 4 digit hex value
+            ST          R0, STORE
+            
+            ; Display space then an x
+            LEA         R0, SPACEX
+            PUTS
+            
+            ; Display the memory contents that were 
+            ; stored at the memory address returned from 
+            ; trap 41
+            
+MEMORY      .FILL       STORE
+            LD          R0, MEMORY
+            TRAP        x41
+            
+            ; Checks to see if this is the last value to be
+            ; printed to the screen, if so end the program
+            ; if it's not continue the program
+            
             ADD         R4, R4, #0001   ; Add to get to the next value needed to display
             NOT         R4, R4          ; Takes the negative R4 and places it into R4
             ADD         R2, R3, R4      ; Add the end input to the current place
@@ -45,12 +83,30 @@ LOOP
             
             HALT
             
+            
+NOTPOSITIVE 
+            ; Should be called when the
+            ; first input is greater than the
+            ; second input
+            
+            LEA         R0, NEWLINE
+            PUTS
+            LEA         R0, NOTP
+            PUTS
+            LEA         R0, NEWLINE
+            PUTS
+            BR          START
+            
+CLEARVALUE  .FILL       x0000
 INPUT1      .BLKW       #1
 INPUT2      .BLKW       #1
+STORE       .BLKW       #1
+NOTP        .STRINGZ    "Make sure the ending value is greater than the starting value"
 PROMPT1     .STRINGZ    "Enter starting memory address: x"
 PROMPT2     .STRINGZ    "Enter ending memory address: x"
 PROMPT3     .STRINGZ    "Memory Contents "
 TOX         .STRINGZ    " to "
+SPACEX      .STRINGZ    " x"
 COLEN       .STRINGZ    ":"
 NEWLINE     .FILL       #10             ; Stores the newline char
 ADDRESS1    .FILL       x0000
@@ -59,10 +115,11 @@ ADDRESS2    .FILL       x0000
             .END
             
             
-            .ORIG       x4000           ; Read a 4 digit hex, check it, and return it in R0
-INPUT            
-
-            ; MAKE SURE THE RETURN IS A 16 DIGIT VALUE
+            .ORIG       x4000           
+INPUT       
+            ; Read a 4 digit hex, check it, and return it in R0
+            
+            ; 
             
             AND         R4, R4, #0      ; Clears R4 to store user input
             JSR         GETCHAR         ; Jumpt to the start of INPUT
@@ -78,6 +135,7 @@ INPUT
             ST          R0, FOURTH
             
             ; Combine all of the chars into one 16 bit value
+            ; by shifting left 4 digits for every char inputed
             AND         R0, R0, #0
             AND         R1, R1, #0
             
@@ -188,7 +246,16 @@ ASSIGNNUM
             
             AND         R2, R2, #0
             
-            ; 0 starts at 48
+            ; Hex 0 starts at ascii 48
+            
+            ; Store the user input in R2 then
+            ; compare it to each number value
+            ; by subtracting 48 initially, compare
+            ; the value to zero. If the value is zero
+            ; then we have our number so we break to the
+            ; correct number function. If the value is 
+            ; anything else (should only be positive) 
+            ; add 1 to get to the next number
             
             ADD         R2, R0, #0      ; Puts user input into R2
             ADD         R2, R2, R1      ; Subtract 48 from user input
@@ -221,6 +288,9 @@ ASSIGNNUM
             ADD         R2, R2, #1
             BRz         NINE
             
+
+            ; Load the approperate numbers binary into 
+            ; R0 then return from call
 
 ZERO        LD          R0, ZER
             RET
@@ -275,11 +345,24 @@ ASSIGNLETTER
             
             ; User input is still in R0
             ; Limit is in R1
+            ; The limit could be for lower and
+            ; upper case letters since the letters
+            ; are stored in order but in two seperate
+            ; places for ascii
             
-            AND         R2, R2, #0
+            ; Store the user input in R2 then
+            ; compare it to each letters value
+            ; by subtracting R1 initially, compare
+            ; the value to zero. If the value is zero
+            ; then we have our letter so we break to the
+            ; correct letters function. If the value is 
+            ; anything else (should only be positive) 
+            ; add 1 to get to the next letter
+            
+            AND         R2, R2, #0      ; Clear R2 to compare input
 
             ADD         R2, R0, #0      ; Puts user input into R2
-            ADD         R2, R2, R1      ; Subtract 48 from user input
+            ADD         R2, R2, R1      ; Subtract R1 from user input
             BRz         AA
             
             ADD         R2, R2, #1
@@ -297,6 +380,9 @@ ASSIGNLETTER
             ADD         R2, R2, #1
             BRz         FF
             
+            
+            ; Load the approperate letters binary into 
+            ; R0 then return from call
             
 AA          LD          R0, A
             RET                         ; Return from call
@@ -345,16 +431,129 @@ INVALID     .STRINGZ    "Invalid input"
             .END
             
             
-            
-            
-            
             .ORIG       x5000
 OUTPUT
+            ; Binary value is stored in R2
+            
+            ST          R2, INPUT3
+            
+            ; Display a 4 digit hex value from a
+            ; 16 bit binary value
+            
             LEA         R0, X
             PUTS
             
+            ; Get the value from the correct space
+            ; in the binary representation
+            
+            LD          R0, INPUT3
+            TRAP        x42             ; Hack shift right trap
+            TRAP        x42
+            TRAP        x42
+            
+            ; Now we have the first char that we
+            ; need to print out in the first
+            ; 4 spaces in R0
+            ; Now we need to match it to the correct
+            ; ascii value to print to the screen
+            
+            JSR         GETCHARS
+            
+            ; Next get the second value from the user
+            
+            LD          R0, INPUT3
+            TRAP        x42             ; Hack shift right trap
+            TRAP        x42
+            JSR         GETCHARS
+            
+            ; For the third input
+            
+            LD          R0, INPUT3
+            TRAP        x42             ; Hack shift right trap
+            JSR         GETCHARS
+            
+            ; For the fourth input
+            
+            LD          R0, INPUT3
+            JSR         GETCHARS
+            
             RTI                         ; Return from interupt
             
+GETCHARS     
+            ; Coverts binary value into ascii
+            ; Checks each possible value then converts it
+            
+            ADD         R0, R0, #0000
+            BRn         ZEROO
+            RET
+            
+            
+ZEROO       LEA         R0, x48
+            PUTS
+            RET
+
+ONEE        LEA         R0, x49
+            PUTS
+            RET
+            
+TWOO        LEA         R0, x50
+            PUTS
+            RET
+            
+THREEE      LEA         R0, x51
+            PUTS
+            RET
+            
+FOURR       LEA         R0, x52
+            PUTS
+            RET
+            
+FIVEE       LEA         R0, x53
+            PUTS
+            RET
+            
+SIXX        LEA         R0, x54
+            PUTS
+            RET
+            
+SEVENN      LEA         R0, x55
+            PUTS
+            RET
+            
+EIGHTT      LEA         R0, x56
+            PUTS
+            RET
+            
+NINEE       LEA         R0, x57
+            PUTS
+            RET
+            
+AAA         LEA         R0, x65
+            PUTS
+            RET
+
+BBB         LEA         R0, x66
+            PUTS
+            RET
+            
+CCC         LEA         R0, x67
+            PUTS
+            RET
+            
+DDD         LEA         R0, x68
+            PUTS
+            RET
+            
+EEE         LEA         R0, x69
+            PUTS
+            RET
+            
+FFF         LEA         R0, x70
+            PUTS
+            RET
+
+INPUT3      .BLKW       #1
+SPACE       .STRINGZ    " "
 X           .STRINGZ    "x"
             .END
             
@@ -364,50 +563,86 @@ X           .STRINGZ    "x"
             .END
             
             
+           .ORIG    x6000
+            ; back up all the registers by pushing them on the system stack
+            ADD     R6, R6, #-1
+            STR     R1, R6, #0
+            ADD     R6, R6, #-1
+            STR     R2, R6, #0
+            ADD     R6, R6, #-1
+            STR     R3, R6, #0
+            ADD     R6, R6, #-1
+            STR     R4, R6, #0
+            ADD     R6, R6, #-1
+            STR     R5, R6, #0
             
-;   What to do in order
-;   
-;   1:  Prompt the user "To enter the starting memory address"
-;
-;   2:  Get user input calling Trap 40
-;
-;   3:  Prompt the user "To enter the ending memory address"
-;
-;   4:  Get user input by calling Trap 40
-;
-;5:  Create Trap x40
-;       Trap x40 is a trap routine that reads a 4 digit hex from the keyboard
-;       and returns the value in R0
-;       This trap is stored at x4000
-;
-;       Fill X40 with input
-;
-;       Verify if Input consists of exactly 4 hex chars, uppercase (lower for ex cred)
-;
-;       Convert ascii to binary, subtract 48 from the ascii rep to get the int rep
-;       ascii for 0 would be #48 and 1 #49
-;
-;6:  Print to the concole "Memory contents from x____ to x____"
-;       The blank spaces are for user input
-;
-;7:  Output the memory contents inputed above by calling Trap 41
-;
-;8:  Create Trap x41
-;       Trap x41 displays the contents of R0 to the disply as a 4 digit
-;       hex value. The reutine should output 5 chars "x____" and the 4 hex digits
-;       Don't display a carriage return as a part of the trap call
-;       This trap is stored at x5000
-;
-;       Fill x41 with output
-;
-;9:  Create memory dump routine
-;
-;       Verify that the starting address is lower than the ending address
-;
-;       Prompt the user to reenter the values if that is not the case
-;       The user also has to reenter the values if the input value isn't proper
-;
-;       
+            ; move R0 into R3.  We'll keep the copy of the 
+            ; unshifted value in R3 as R0 gets used for all kinds
+            ; of scratch stuff and/or sending parameters to other
+            ; traps
             
+            AND     R3, R3, #0
+            ADD     R3, R3, R0
+   
+            LD      R1, MASK_TEST
+            LD      R5, MASK_WRITE
+            AND     R4, R4, #0       ; I'm going to assembble the answer in R4.
+                                     ; it should be cleared initally 
+                                     
             
+            LD      R2, LOOP_COUNT
+LOOP2       AND     R0, R3, R1
+            BRz     ELSE
+            JSR     P1
+            BRnzp   CONTINUE
+ELSE        JSR     P0
+CONTINUE    ADD     R1, R1, R1
+            ADD     R5, R5, R5
+            ADD     R2, R2, #-1
+            BRnp    LOOP2
+
+            ; I'm done.  Copy the created answer into R0 so it can be
+            ; "returned" to the caller.  Restore all the other registers
+            ; from the system stack and then return from TRAP call
+DONE        AND     R0, R0, #0
+            ADD     R0, R0, R4
+            LDR     R5, R6, #0
+            ADD     R6, R6, #1
+            LDR     R4, R6, #0
+            ADD     R6, R6, #1
+            LDR     R3, R6, #0
+            ADD     R6, R6, #1
+            LDR     R2, R6, #0
+            ADD     R6, R6, #1
+            LDR     R1, R6, #0
+            ADD     R6, R6, #1
+            RTI
+
+P0          ; debug code LD      R0, ASCII_0
+            ; debug code OUT     
+            RET
+
+P1          ; debug code LD      R0, ASCII_1
+            ; debug code OUT
+            ADD     R4, R4, R5
+            RET
             
+ASCII_0     .FILL   #48
+ASCII_1     .FILL   #49
+MASK_TEST   .FILL   x0002
+MASK_WRITE  .FILL   x0001
+LOOP_COUNT  .FILL   xF
+            .END
+            
+            .ORIG   x42
+            .FILL   x6000
+            .END
+            
+           
+
+
+
+
+
+
+
